2db4f63f38f207b5fd704d019ea527c075a252c8
arminhammer <arminhammer@gmail.com>
2015-07-28 21:58:53 -0400

    Getting the initial struture up and running, and testing working.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,17 @@
 'use strict';
-import assert from 'assert';
-import wolkenFormLib from '../lib';
 
-describe('wolken-form-lib', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+import assert from 'assert';
+import * as Wolken from '../lib/index';
+
+console.log('Creating template...');
+var template = new Wolken.Template();
+console.log('Created.');
+
+describe('functions', function () {
+  it('should be able to call a function', function () {
+    template.build(function(result) {
+      assert.equal('{"AWSTemplateFormatVersion":"2010-09-09","Description":"","Parameters":{},"Mappings":{},"Conditions":{},"Resources":{},"Outputs":{}}', result);
+    });
   });
 });
+
