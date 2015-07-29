d8a615e479101c7d356171687328bf54ed599466
arminhammer <arminhammer@gmail.com>
2015-07-29 00:07:52 -0400

    More scaffolding of the template classes.

--- test/index.js
+++ test/index.js
@@ -2,16 +2,35 @@
 
 import assert from 'assert';
 import * as Wolken from '../lib/index';
+import AWS from 'aws-sdk';
 
-console.log('Creating template...');
 var template = new Wolken.Template();
-console.log('Created.');
 
-describe('functions', function () {
-  it('should be able to call a function', function () {
-    template.build().then(function(result) {
-      assert.equal('{"AWSTemplateFormatVersion":"2010-09-09","Description":"","Parameters":{},"Mappings":{},"Conditions":{},"Resources":{},"Outputs":{}}', result);
+describe('build', function () {
+  it('should be able to call the build function', function () {
+    return template.build().then(function(result) {
+      assert.strictEqual(result, '{"AWSTemplateFormatVersion":"2010-09-09","Description":"","Metadata":{},"Parameters":{},"Mappings":{},"Conditions":{},"Resources":{},"Outputs":{}}');
     });
   });
 });
 
+describe('build', function () {
+  it('The output template should be a valid CF template', function () {
+    return template.build().then(function(result) {
+      var params = {
+        TemplateBody: result
+      };
+      var cloudformation = new AWS.CloudFormation();
+      return cloudformation.validateTemplate(params, function(err, data) {
+        if(err) {
+          console.log(err);
+          return assert(false, 'The template did not validate.');
+        }
+        if(data)  {
+          console.log(data);
+          return assert(true, 'The template validated.');
+        }
+      });
+    });
+  });
+});
